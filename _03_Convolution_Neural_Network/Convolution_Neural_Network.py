9ae9666444feb77a0688fcfb48dd4acaf886b5ef
Dolores-Ww <131885110+Dolores-Ww@users.noreply.github.com>
2023-06-29 18:37:55 +0800

    Update Convolution_Neural_Network.py

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -2,51 +2,41 @@
 # 在自己电脑上训练好模型，保存参数，在这里读取模型参数（不要使用JIT读取），在main中返回读取了模型参数的模型
 
 import os
-
-os.system("sudo pip3 install torch")
-os.system("sudo pip3 install torchvision")
-
 import torch
 import torch.nn as nn
-import torch.optim as optim
 import torchvision
-
 from torch.utils.data import DataLoader
-    
+
 class NeuralNetwork(nn.Module):
     def __init__(self):
-        super(NeuralNetwork, self).__init__()  # Corrected line
-        self.resnet18 = torchvision.models.resnet18(pretrained=True) # 调用预训练的ResNet18模型
-    # 冻结ResNet中的参数，避免训练时梯度反向传递影响到预训练的模型
+        super(NeuralNetwork, self).__init__()
+        self.resnet18 = torchvision.models.resnet18(pretrained=True)
         for param in self.resnet18.parameters():
-            param.requires_grad = False   # 重新定义ResNet的输出层
+            param.requires_grad = False
         self.fc = nn.Linear(512, 10)
-    
+
     def forward(self, x):
         x = self.resnet18(x)
         return x
-        
+
 def read_data():
-    # 这里可自行修改数据预处理，batch大小也可自行调整
-    # 保持本地训练的数据读取和这里一致
     dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True, transform=torchvision.transforms.ToTensor())
     dataset_val = torchvision.datasets.CIFAR10(root='../data/exp03', train=False, download=False, transform=torchvision.transforms.ToTensor())
     data_loader_train = DataLoader(dataset=dataset_train, batch_size=256, shuffle=True)
     data_loader_val = DataLoader(dataset=dataset_val, batch_size=256, shuffle=False)
-      # 保存模型参数
-    torch.save(model.state_dict(), '../pth/model.pth')
     return dataset_train, dataset_val, data_loader_train, data_loader_val
 
-
-    
-  
-    
 def main():
     dataset_train, dataset_val, data_loader_train, data_loader_val = read_data()
-    model = NeuralNetwork() # 若有参数则传入参数
-    train(model, data_loader_train, data_loader_val, epochs=10, learning_rate=0.001)
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    parent_dir = os.path.dirname(current_dir)
-    model.load_state_dict(torch.load('model.pth'))  # 修改该行为加载参数的路径
+    model = NeuralNetwork()
+
+    # 保存模型参数
+    path = '../pth/model.pth'
+    torch.save(model.state_dict(), path)
+
+    # 加载模型参数
+    model.load_state_dict(torch.load(path))
+
     return model
+
     
