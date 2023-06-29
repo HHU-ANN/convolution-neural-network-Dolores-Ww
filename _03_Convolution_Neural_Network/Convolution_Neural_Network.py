22cdf1afa95aadbf575190a0969bfd2336ee5943
Dolores-Ww <131885110+Dolores-Ww@users.noreply.github.com>
2023-06-29 22:57:11 +0800

    Update Convolution_Neural_Network.py

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -11,42 +11,39 @@ class NeuralNetwork(nn.Module):
 
         # Define your neural network architecture here
 
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=4, padding=1)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1)
         self.relu1 = nn.ReLU()
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=4, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
         self.relu2 = nn.ReLU()
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=4, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1)
         self.relu3 = nn.ReLU()
         self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.fc = nn.Linear(28* 28 * 256, 10) # Output layer
 
-   def forward(self, x):
-    # Implement the forward pass of your neural network here
+    def forward(self, x):
+        # Implement the forward pass of your neural network here
 
-    x = self.conv1(x)
-    x = self.relu1(x)
-    x = self.pool1(x)
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.pool1(x)
 
-    x = self.conv2(x)
-    x = self.relu2(x)
-    x = self.pool2(x)
-    
-    x = self.conv3(x)
-    x = self.relu3(x)
-    x = self.pool3(x)
-    
-    # Calculate the final feature map size
-    feature_size = math.ceil(x.size(2) / 2 / 2 / 2)  # Divide by 2 for every pooling layer
-    
-    x = x.view(-1, feature_size * feature_size * 256)  # Flatten the input tensor
-    x = self.fc(x)
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.pool2(x)
 
-    return x
+        x = self.conv3(x)
+        x = self.relu3(x)
+        x = self.pool3(x)
+
+        x = x.view(-1, 28 * 28 * 256) # Flatten the input tensor
+        x = self.fc(x)
+
+        return x
 
 
 
