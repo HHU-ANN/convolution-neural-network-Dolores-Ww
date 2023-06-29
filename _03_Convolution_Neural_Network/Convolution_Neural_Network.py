7ad4419585a1e00501843635523a3fd29052437e
Dolores-Ww <131885110+Dolores-Ww@users.noreply.github.com>
2023-06-29 21:17:48 +0800

    Update Convolution_Neural_Network.py

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -1,6 +1,3 @@
-# 在该文件NeuralNetwork类中定义你的模型 
-# 在自己电脑上训练好模型，保存参数，在这里读取模型参数（不要使用JIT读取），在main中返回读取了模型参数的模型
-
 import os
 import torch
 import torch.nn as nn
@@ -10,14 +7,12 @@ from torch.utils.data import DataLoader
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
-        # Define your model architecture here
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
         self.relu = nn.ReLU()
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.fc = nn.Linear(16 * 8 * 8, 10)
 
     def forward(self, x):
-        # Implement the forward pass of your model
         x = self.conv1(x)
         x = self.relu(x)
         x = self.pool(x)
@@ -27,16 +22,15 @@ class NeuralNetwork(nn.Module):
 
 
 def read_data():
-    dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True, transform=torchvision.transforms.ToTensor())
-    dataset_val = torchvision.datasets.CIFAR10(root='../data/exp03', train=False, download=False, transform=torchvision.transforms.ToTensor())
+    dataset_train = torchvision.datasets.CIFAR10(root='./data/exp03', train=True, download=True, transform=torchvision.transforms.ToTensor())
+    dataset_val = torchvision.datasets.CIFAR10(root='./data/exp03', train=False, download=False, transform=torchvision.transforms.ToTensor())
     data_loader_train = DataLoader(dataset=dataset_train, batch_size=256, shuffle=True)
-    data_loader_val = DataLoader(dataset=dataset_val, batch_size=1, shuffle=False)  # 将测试数据的batch_size设置为1
+    data_loader_val = DataLoader(dataset=dataset_val, batch_size=1, shuffle=False)
     return dataset_train, dataset_val, data_loader_train, data_loader_val
 
 
-
 def main():
-    model = NeuralNetwork()  # 若有参数则传入参数
+    model = NeuralNetwork()
     dataset_train, dataset_val, data_loader_train, data_loader_val = read_data()
     parent_dir = os.path.dirname(os.path.abspath(__file__))
     model_dir = os.path.join(parent_dir, 'pth')
@@ -48,4 +42,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
     
