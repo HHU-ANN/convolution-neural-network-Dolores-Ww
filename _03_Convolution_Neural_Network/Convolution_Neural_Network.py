0c83bd373e491a12f0e429b29a6a956fcb058309
Dolores-Ww <131885110+Dolores-Ww@users.noreply.github.com>
2023-06-30 12:03:00 +0800

    Update Convolution_Neural_Network.py

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -1,43 +1,31 @@
 import os
 import torch
 import torch.nn as nn
-import torch.optim as optim
-import torchvision.transforms as transforms
-import torchvision.datasets as datasets
 import torchvision
-import math
 from torch.utils.data import DataLoader
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
-        self.relu1 = nn.ReLU(inplace=True)
-        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc1 = nn.Linear(16 * 16 * 32, 128)
-        self.relu2 = nn.ReLU(inplace=True)
-        self.fc2 = nn.Linear(128, 10)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc = nn.Linear(64 * 8 * 8, 10)
 
     def forward(self, x):
         x = self.conv1(x)
-        x = self.relu1(x)
-        x = self.pool1(x)
+        x = self.relu(x)
+        x = self.pool(x)
         x = x.view(x.size(0), -1)
-        x = self.fc1(x)
-        x = self.relu2(x)
-        x = self.fc2(x)
+        x = self.fc(x)
         return x
-        
-model = NeuralNetwork()
-# 定义优化器和损失函数
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+
 
 def read_data():
     dataset_train = torchvision.datasets.CIFAR10(root='./data/exp03', train=True, download=True, transform=torchvision.transforms.ToTensor())
     dataset_val = torchvision.datasets.CIFAR10(root='./data/exp03', train=False, download=False, transform=torchvision.transforms.ToTensor())
     data_loader_train = DataLoader(dataset=dataset_train, batch_size=256, shuffle=True)
-    data_loader_val = DataLoader(dataset=dataset_val, batch_size=256, shuffle=False)
+    data_loader_val = DataLoader(dataset=dataset_val, batch_size=1, shuffle=False)
     return dataset_train, dataset_val, data_loader_train, data_loader_val
 
 
@@ -54,5 +42,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-
     
