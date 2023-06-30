3b6366b879a0cae8cc68208ffbc5fa6b6e29dfcc
Dolores-Ww <131885110+Dolores-Ww@users.noreply.github.com>
2023-06-30 11:07:03 +0800

    Update Convolution_Neural_Network.py

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -8,45 +8,26 @@ from torch.utils.data import DataLoader
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
-
-        # Define your neural network architecture here
-
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=512, kernel_size=3, padding=1)
-        self.relu1 = nn.ReLU()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.relu1 = nn.ReLU(inplace=True)
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        self.conv2 = nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=3, padding=1)
-        self.relu2 = nn.ReLU()
-        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        self.conv3 = nn.Conv2d(in_channels=1024, out_channels=2048, kernel_size=3, padding=1)
-        self.relu3 = nn.ReLU()
-        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        self.fc = nn.Linear(32 * 32 * 32, 10) # Output layer
+        self.fc1 = nn.Linear(16 * 16 * 32, 128)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
-        # Implement the forward pass of your neural network here
-
         x = self.conv1(x)
         x = self.relu1(x)
         x = self.pool1(x)
-
-        x = self.conv2(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
         x = self.relu2(x)
-        x = self.pool2(x)
-
-        x = self.conv3(x)
-        x = self.relu3(x)
-        x = self.pool3(x)
-
-        
-        x = x.view(-1, 32 * 32* 32) # Flatten the input tensor
-        x = self.fc(x)
-
+        x = self.fc2(x)
         return x
 
-
+# 定义优化器和损失函数
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=learning_rate)
 
 def read_data():
     dataset_train = torchvision.datasets.CIFAR10(root='./data/exp03', train=True, download=True, transform=torchvision.transforms.ToTensor())
