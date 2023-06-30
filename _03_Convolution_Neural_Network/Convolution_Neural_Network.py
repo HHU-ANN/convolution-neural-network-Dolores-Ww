a53acbe3e7a3ad68129bfbbd5dd3e87631acfd09
Dolores-Ww <131885110+Dolores-Ww@users.noreply.github.com>
2023-06-30 13:40:12 +0800

    Update Convolution_Neural_Network.py

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -1,37 +1,89 @@
+# 在该文件NeuralNetwork类中定义你的模型 
+# 在自己电脑上训练好模型，保存参数，在这里读取模型参数（不要使用JIT读取），在main中返回读取了模型参数的模型
+
 import os
 import torch
 import torch.nn as nn
+import torch.optim as optim
 import torchvision
 from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.nn.functional as F
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.relu = nn.ReLU()
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.fc = nn.Linear(64 * 8 * 8, 10)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.fc1 = nn.Linear(128 * 32 * 32, 256)
+        self.fc2 = nn.Linear(256, 10)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.relu(x)
-        x = self.pool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = x.view(-1, 128 * 32 * 32)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
         return x
 
 
+# 加载CIFAR-10数据集
+transform = torchvision.transforms.Compose([
+    torchvision.transforms.RandomHorizontalFlip(),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+])
 def read_data():
-    dataset_train = torchvision.datasets.CIFAR10(root='./data/exp03', train=True, download=True, transform=torchvision.transforms.ToTensor())
-    dataset_val = torchvision.datasets.CIFAR10(root='./data/exp03', train=False, download=False, transform=torchvision.transforms.ToTensor())
-    data_loader_train = DataLoader(dataset=dataset_train, batch_size=256, shuffle=True)
-    data_loader_val = DataLoader(dataset=dataset_val, batch_size=1, shuffle=False)
-    return dataset_train, dataset_val, data_loader_train, data_loader_val
+    train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
+    test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False)
+    return train_dataset,train_loader,test_dataset,test_loader
+# 初始化模型和定义损失函数与优化器
+model = NeuralNetwork()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+train_dataset,train_loader,test_dataset,test_loader = read_data()
+
+# 训练循环
+num_epochs = 10
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
+model.train()
+
+for epoch in range(num_epochs):
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+
+        inputs, labels = data
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+
+        optimizer.zero_grad()
+
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+        if i % 200 == 199:
+            print(
+                f'Epoch [{epoch + 1}/{num_epochs}], Batch [{i + 1}/{len(train_loader)}], Loss: {running_loss / 200:.4f}')
+            running_loss = 0.0
+
+# 保存模型参数
+torch.save(model.state_dict(), 'model.pth')
 
 
 def main():
     model = NeuralNetwork()
-    dataset_train, dataset_val, data_loader_train, data_loader_val = read_data()
     parent_dir = os.path.dirname(os.path.abspath(__file__))
     model_dir = os.path.join(parent_dir, 'pth')
     os.makedirs(model_dir, exist_ok=True)
@@ -42,4 +94,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
